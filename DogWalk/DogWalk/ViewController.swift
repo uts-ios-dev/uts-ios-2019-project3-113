6e28e27c7679f8d5c2b3251335951a355f426343
NickEsse <nicholaskain@gmail.com>
2019-06-03 14:57:37 +1000

    Segue for list view working.

--- DogWalk/DogWalk/ViewController.swift
+++ DogWalk/DogWalk/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // Test vince
     }
 
     @IBAction func introLoginButton(_ sender: Any) {
